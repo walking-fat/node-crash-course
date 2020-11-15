de403a63742aec2252922782b1e1cdfb3c872fa9
Zeng Chen <chenzeng0112@gmail.com>
2020-11-14 22:07:16 -0600

    Tutorial #4 commit_1

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    // console.log('request made');
+    console.log(req.url, req.method);
+
+    // set header content type
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.write('Hello');
+
+    res.setHeader('Content-Type', 'text/html');
+
+
+    res.write('<head><link rel="stylesheet" href="#"></head>')
+
+    res.write("<p>Hello MOFO, WHAT'S MY NAME?</p>");
+
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
