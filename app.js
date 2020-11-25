ca367beeb85dae43ea522543213a7c0b32a7ebf0
Zeng Chen <chenzeng0112@gmail.com>
2020-11-24 21:34:38 -0600

    Tutorial #8 middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -9,6 +10,11 @@ app.set('views', 'myviews');
 
 // listen for requests
 app.listen(3000);
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
 
 app.get('/', (req, res) => {
     // res.send('<p>Home Page</p>');
