030eac271d35dfc7f0b182a8234f3b5cc33ec398
Zeng Chen <chenzeng0112@gmail.com>
2020-11-29 19:15:01 -0600

    Tutorial #10
    get post delete

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
+const { render } = require('ejs');
+const { result } = require('lodash');
 
 // express app
 const app = express();
@@ -21,6 +23,7 @@ app.set('views', 'myviews');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // mongoose and mongo sandbox routes
@@ -89,6 +92,11 @@ app.get('/about', (req, res) => {
 });
 
 // blog routes
+// create blog
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create a new Blog'});
+});
+
 app.get('/blogs', (req, res) => {
     Blog.find().sort({ createdAt: -1 })
         .then((result) => {
@@ -99,15 +107,48 @@ app.get('/blogs', (req, res) => {
         })
 });
 
+// POST request
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then((result) => {
+            res.render('details', { blog: result, title: 'Blog Details' })
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+// delete
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs' })
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
 // // redirect
 // app.get('/about-us', (req, res) => {
 //     res.redirect('/about');
 // });
 
-// create blog
-app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create a new Blog'});
-})
+
 
 // 404 page
 app.use((req, res) => {
